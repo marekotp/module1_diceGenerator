15086b4103141ac9ca03583942a6b0a9bd2e9f36
marekotp <marekotp@gmail.com>
2022-10-22 14:06:31 +1300

    Dice Roll Function Added, Dropdown added, Dice value changes when new dice selected.

--- Website/diceRollTest.js
+++ Website/diceRollTest.js
@@ -1,8 +1,17 @@
 let dice = {
+    value: 6,
     roll: function () {
-      let randomNumber = Math.floor(Math.random() * 6) + 1;
+      let randomNumber = Math.floor(Math.random() * this.value) + 1;
       return randomNumber;
     }
+  }
+
+  function newRoll(val){
+    dice.value = val
+    let result = dice.roll();
+    printNumber (result);
+
+    console.log("diceroll:" + val)
   }
 
   function printNumber(number) {
